77681036a2450a51c9150e97948d63ddf75be6a9
Milinda <rockmakas@gmail.com>
2014-12-28 13:42:44 +0530

    wordpress image upload init

--- magento.php
+++ magento.php
@@ -7,41 +7,44 @@ Version: 1.0
 Author: Makas Rock
 Author URI: http://stunningcodes.com
 */
-
-
-add_action('add_meta_boxes','boj_mbe_create');
-	
-	function boj_mbe_create() {
-		add_meta_box( 'boj-meta','My Custom Meta Box','boj_mbe_function','post','normal','high');
-	}
-	function boj_mbe_function() {
-		$boj_mbe_name = get_post_meta( $post-> ID, '_boj_mbe_named', true );
-		$boj_mbe_costume = get_post_meta( $post-> ID, '_boj_mbe_costumed', true );
-		echo 'Please fill out the information belowd';
-	?>
-	<p> Name: <input type="text" name="boj_mbe_name" value="<?php echo esc_attr( $boj_mbe_name ); ?> " / > </p>
-	<p> Costume:
-		<select name="boj_mbe_costume" >
-			<option value="vampire" <?php selected( $boj_mbe_costume, 'vampire' ); ?> >Vampire</option>
-			<option value="zombie" <?php selected( $boj_mbe_costume,'zombie' ); ?> >Zombie</option >
-			<option value="smurf" <?php selected( $boj_mbe_costume, 'smurf' ); ?> >Smurf</option>
-		</select>
-	</p>
-	<?php
-	}
-	//Nothings done
-
-add_action( 'save_post', 'boj_mbe_save_meta' );
-
-function boj_mbe_save_meta( $post_id ) {
-//verify the metadata is set
-	if ( isset( $_POST['boj_mbe_name'] ) ) {
-	//save the metadata
-		update_post_meta( $post_id,'_boj_mbe_name',strip_tags($_POST['boj_mbe_name'] ) );
-
-		update_post_meta( $post_id,'_boj_mbe_costume',strip_tags($_POST['boj_mbe_costume'] ) );
-
-	}
+add_action( 'admin_init', 'boj_mbe_image_create' );
+function boj_mbe_image_create() {
+//create a custom meta box
+add_meta_box( 'boj-image-meta', 'Set Image', 'boj_mbe_image_function', 'post',
+'normal', 'high' );
 }
+function boj_mbe_image_function( $post ) {
+//retrieve the metadata value if it exists
+$boj_mbe_image = get_post_meta( $post-> ID, '_boj_mbe_image', true );
+?>
+Image <input id="boj_mbe_image" type="text" size="75" name="boj_mbe_image" value=" <?php echo esc_url( $boj_mbe_image ); ?> " / >
+<input id="upload_image_button" type="button" value="Media Library Image" class="button-secondary" / >
+<p> Enter an image URL or use an image from the Media Library </p>
+<?php
+}
+//script actions with page detection
+add_action('admin_print_scripts-post.php', 'boj_mbe_image_admin_scripts');
+add_action('admin_print_scripts-post-new.php', 'boj_mbe_image_admin_scripts');
+function boj_mbe_image_admin_scripts() {
+wp_enqueue_script( 'boj-image-upload',
+plugins_url( '/magento/boj-meta-image.js' ),
+array( 'jquery','media-upload','thickbox' ) );
+}
+//style actions with page detection
+add_action('admin_print_styles-post.php', 'boj_mbe_image_admin_styles');
+add_action('admin_print_styles-post-new.php', 'boj_mbe_image_admin_styles');
+function boj_mbe_image_admin_styles() {
+wp_enqueue_style( 'thickbox' );
+}
+//hook to save the meta box data
+	add_action( 'save_post', 'boj_mbe_image_save_meta' );
+	
+	function boj_mbe_image_save_meta( $post_id ) {
+//verify the metadata is set
 
+	if ( isset( $_POST['boj_mbe_image'] ) ) {
+	//save the metadata
+		update_post_meta( $post_id, '_boj_mbe_image',esc_url( $_POST['boj_mbe_image'] ) );
+	}
+	}
 ?>
