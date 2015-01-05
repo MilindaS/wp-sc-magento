59a4bbeed6288ddb49a3c989906281b8011788ab
Milinda <milinda.s@ceynet.asia>
2015-01-05 12:20:23 +0530

    Chaned api

--- inc/sc-magento-api.class.php
+++ inc/sc-magento-api.class.php
@@ -6,6 +6,50 @@ class SC_Api {
     static public function init(){
         SC_Api::display();
     }
+    
+    static public function getProduct(){
+        //return 1;
+        $rest_url = get_option('rest_url');
+        $rest_username = get_option('rest_username');
+        $rest_api_key = get_option('rest_api_key');
+
+
+        $curl = curl_init($rest_url);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        $curl_response = curl_exec($curl);
+
+        if ($curl_response === false) {
+            $info = curl_getinfo($curl);
+            curl_close($curl);
+            die('error occured during curl exec. Additioanl info: ' . var_export($info));
+        }
+
+        curl_close($curl);
+        $decoded = json_decode($curl_response,true);
+        if (isset($decoded->response->status) && $decoded->response->status == 'ERROR') {
+            die('error occured: ' . $decoded->response->errormessage);
+        }
+        //echo 'response ok!';
+        //var_export($decoded->response);
+        $content = '<div class="sc-row">';
+        foreach($decoded as $item){
+            //$content.=$item['entity_id'];
+            $content .= '<div class="sc-widget-panel">
+                            <div id="sc-widget-container-1">    
+                                <center>
+                                    <div class="sc-row"><img src="'.$item["image_url"].'" alt="" id="sc-widget-img"></div>
+                                    <span id="sc-widget-title">'.$item["name"].'</span>
+                                    <div id="sc-widget-price">SLRs '.$item["final_price_with_tax"].'.00</div>
+                                    <button id="sc-widget-button">View Product</button>
+                                </center>
+                            </div>
+                        </div>';
+        }
+        $content .= '</div>';
+        //print_r($decoded);
+
+        return $content;
+    }
 
     static public function display(){
         ?>
@@ -49,38 +93,6 @@ class SC_Api {
     }
 
 
-    static public function getProduct(){
-        //return 1;
-        $rest_url = get_option('rest_url');
-        $rest_username = get_option('rest_username');
-        $rest_api_key = get_option('rest_api_key');
-
-
-        $curl = curl_init($rest_url);
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-        $curl_response = curl_exec($curl);
-
-        if ($curl_response === false) {
-            $info = curl_getinfo($curl);
-            curl_close($curl);
-            die('error occured during curl exec. Additioanl info: ' . var_export($info));
-        }
-
-        curl_close($curl);
-        $decoded = json_decode($curl_response,true);
-        if (isset($decoded->response->status) && $decoded->response->status == 'ERROR') {
-            die('error occured: ' . $decoded->response->errormessage);
-        }
-        //echo 'response ok!';
-        //var_export($decoded->response);
-        $content = '';
-        foreach($decoded as $item){
-            $content.=$item['entity_id'];
-        }
-        print_r($decoded);
-//echo 1;
-        //return $decoded;
-    }
-
+    
 }
 ?>
